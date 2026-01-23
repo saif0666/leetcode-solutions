96bed6a852ddd45dac5caa4499b2858b2e05335f
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-01-23 10:18:21 +0530

    Time: 1 ms (95.66%), Space: 42.2 MB (95.58%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,21 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-           if(n<1){
-            return false;
-           }
-           else if(n == 1){
-            return true;
-           }else{
-            while(n%2 == 0){
-                n /= 2;
-            }
-            if(n == 1){
-                return true;
-            }
-            else{
-                return false;
-            }
-           }
+         return n>0 && (n &(n-1))==0;
 
     }
 }
