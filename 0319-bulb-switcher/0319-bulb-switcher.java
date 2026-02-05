65e70a79d2777e2a67ab682eaad313ab9a1e6d07
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-02-05 12:30:51 +0530

    Time: 0 ms (100%), Space: 42 MB (83.8%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,15 @@
+//saif0666 
+class Solution {
+    public int bulbSwitch(int n) {
+        int count = 0;
+        int i = 1;
+        while(i*i<=n){
+            count++;
+            i++;
+           
+        }
+        return count;
+        // return n;
+        
+    }
+}
