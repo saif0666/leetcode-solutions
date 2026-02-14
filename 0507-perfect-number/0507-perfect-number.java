1b46a5d5461ef3e6c5b7426c524550b957364fa6
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-02-14 16:57:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (42.99%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num<=1){
-            return false;
-        }
-        int sum = 1;
-        for(int i = 2;i*i<num;i++){
-            if(num%i==0){
-                sum += i;
-                if(num/i != i){
-                    sum += num/i;
-                }
-            }
-        } 
+        // if(num<=1){
+        //     return false;
+        // }
+        // int sum = 1;
+        // for(int i = 2;i*i<num;i++){
+        //     if(num%i==0){
+        //         sum += i;
+        //         if(num/i != i){
+        //             sum += num/i;
+        //         }
+        //     }
+        // } 
              
-        return sum==num;
+        // return sum==num;
+        return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336;
     }
 }
