5e63eb7ac1c6fbbfa5f3e6006494335ce59761c5
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-02-25 19:11:55 +0530

    Time: 4 ms (73.47%), Space: 47 MB (61.05%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -4,9 +4,9 @@ class Solution {
         int oper = 0;
         for(int i = 1;i<nums.length;i++){
             if(nums[i]<=nums[i-1]){
-                int need = nums[i-1] - nums[i] + 1;
-                oper += need;
-                nums[i] += need; 
+                int need = nums[i-1]  + 1;
+                oper += need-nums[i];
+                nums[i] = need; 
             }
             
         }
