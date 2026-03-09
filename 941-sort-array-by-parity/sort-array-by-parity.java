29ab8914d7a9b67a541bba2066411288c0310724
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-03-09 22:25:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (84.54%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,23 @@
+//saif0666
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int l = 0, r = nums.length-1;
+        while(l<r){
+            if(nums[l]%2==0){
+                l++;
+            }
+            else if(nums[r]%2==1){
+                r--;
+            }
+            else{
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+                r--;
+            }
+        }
+      return nums;  
+    }
+}
