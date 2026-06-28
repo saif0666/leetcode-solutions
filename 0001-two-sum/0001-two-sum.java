cb4744ff6690583171f308432ee3a175680858a8
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-06-28 22:16:14 +0530

    Time: 50 ms (5.87%), Space: 47 MB (72.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,14 @@
-//saif0666
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        int[] sum = new int[2];
-       
+        int[] soln = {0,0};
         for(int i = 0; i < nums.length;i++){
-            for(int j = i+1; j < nums.length;j++){
-          
-                if (nums[i] + nums[j] == target){
-                    sum[0] = i;
-                    sum[1] = j;
-                    break;
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    soln[0] = i;
+                    soln[1] = j;
                 }
-            } 
+            }
         }
-        return sum;
-          
+        return soln;
     }
 }
