1a315b0e2ffb43fb3c55d3f69fd1543608485512
Saif Ahmad <148641088+saif0666@users.noreply.github.com>
2026-09-07 09:33:56 +0530

    Time: 7 ms (99.08%), Space: 48.6 MB (95.75%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+      Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+      int n = intervals.length;
+      int [][] ans = new int [n][2];
+      int j = 0;
+      ans[0] = intervals[0];
+      for(int i = 1;i<n;i++){
+        if(ans[j][1]>=intervals[i][0]){ans[j][1]=Math.max(ans[j][1],intervals[i][1]);
+        }
+        else{
+            j++;
+            ans[j]=intervals[i];
+        }
+      }
+    return Arrays.copyOf(ans,j+1);
+    }
+}
